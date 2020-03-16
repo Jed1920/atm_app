81d45357937bcbf0f65e1b911f35136ddd49ca82
Jed Johnson <jed.johnson@softwire.com>
2020-03-16 18:02:56 +0000

    Start controller
    Created "endpoints" for: 
    Requesting total
    Depositing notes
    Withdrawing (not functional)

--- src/main/AtmApplication.java
+++ src/main/AtmApplication.java
@@ -1,8 +1,44 @@
 package main;
 
+import java.util.Scanner;
+
 public class AtmApplication {
 
     public static void main(String[] args){
-        System.out.print("Hello World");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter number of $20 notes available");
+        int numberTwenties = Integer.parseInt(input.nextLine());
+        System.out.println("Enter number of $50 notes available");
+        int numberFifties = Integer.parseInt(input.nextLine());
+
+        boolean run = true;
+
+        while(run) {
+            System.out.println("What service do you require: Deposit, Withdraw, Total");
+            String service = input.nextLine();
+
+            switch (service) {
+                case "Deposit":
+                    System.out.println("Enter number of $20 to deposit");
+                    int numberNewTwenties = Integer.parseInt(input.nextLine());
+                    numberTwenties = numberNewTwenties + numberTwenties;
+
+                    System.out.println("Enter number of $50 to deposit");
+                    int numberNewFifties = Integer.parseInt(input.nextLine());
+                    numberFifties = numberNewFifties + numberFifties;
+                    break;
+                case "Withdraw":
+                    System.out.println("Enter desired amount in $");
+                    int requestedAmount = Integer.parseInt(input.nextLine());
+                    break;
+                case "Total":
+                    System.out.println(String.format("Number of $20 notes: %d", numberTwenties));
+                    System.out.println(String.format("Number of $50 notes: %d", numberFifties));
+                    int total = numberFifties * 50 + numberTwenties * 20;
+                    System.out.println(String.format("Total: $%d", total));
+                    break;
+            }
+        }
     }
 }
